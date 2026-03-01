d0f1192c9e8e1aec9e46ac1b129bb8b65aa89e12
KhristSho <ShonusKhristo@yandex.ru>
2026-03-01 17:33:55 +0700

    init commit

---
+++ eslint.config.js
@@ -0,0 +1,66 @@
+import js from "@eslint/js";
+import globals from "globals";
+
+export default [
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.es2024,
+      },
+    },
+    rules: {
+      "no-undef": "error",
+      "no-unused-vars": "warn",
+      "no-console": "off",
+      "no-const-assign": "error",
+      "no-duplicate-case": "error",
+      "no-empty": "warn",
+      "no-extra-semi": "warn",
+      "no-func-assign": "error",
+      "no-implicit-globals": "error",
+      "no-invalid-regexp": "error",
+      "no-irregular-whitespace": "error",
+      "no-misleading-character-class": "error",
+      "no-sparse-arrays": "error",
+      "no-unexpected-multiline": "error",
+      "no-unreachable": "error",
+      "no-unsafe-finally": "error",
+      "no-useless-escape": "warn",
+      "require-atomic-updates": "error",
+      "use-isnan": "error",
+      "valid-typeof": "error",
+      "semi": ["warn", "always"],
+      "indent": ["warn", 2],
+      "eqeqeq": ["warn", "always"],
+      "quotes": ["error", "double"]
+    },
+  },
+  {
+    files: ["**/*.test.js"],
+    languageOptions: {
+      globals: {
+        ...globals.jest,
+      },
+    },
+    plugins: {
+      
+    },
+    rules: {
+      "jest/prefer-expect-assertions": "off"
+    }
+  },
+  js.configs.recommended, 
+  {
+    ignores: [
+      "**/coverage/**",
+      "**/node_modules/**",
+      "**/dist/**",
+      "/src/js/arraybuffers/getbuffer.js",
+    ],
+  }
+];
